64fdf4dab5713bb1524abb68e7d18814dd17c512
fengxiaosai <58321221+fengxiaosai@users.noreply.github.com>
2019-12-28 15:07:47 +0800

    菜单、工具栏、状态栏大综合

---
+++ 34_PutTogether.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+在这个例子中,
+综合例31、32、33
+将创建一个菜单条,工具栏和状态栏的小窗口
+"""
+
+import sys
+from PyQt5.QtWidgets import QMainWindow, QTextEdit, QAction, QApplication
+from PyQt5.QtGui import QIcon
+
+
+class Example(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        textEdit = QTextEdit() # 创建了一个QTextEdit,并把他设置为窗口的布局
+        self.setCentralWidget(textEdit)
+
+        exitAction = QAction(QIcon('exit24.png'), 'Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(self.close)
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+
+        toolbar = self.addToolBar('Exit')
+        toolbar.addAction(exitAction)
+
+        self.setGeometry(300, 300, 350, 250)
+        self.setWindowTitle('Main window')
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
